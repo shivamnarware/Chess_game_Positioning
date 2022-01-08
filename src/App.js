362807f6c3530f70247a204d853b568fbd2145f6
shivamnarware <shivamnarware97@gmail.com>
2022-01-08 18:01:43 +0530

    "Added_the_board"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,106 @@
 import react from "react";
-
+import './App.css'
 function App() {
   return (
-     <div>
-       <h1>Hello</h1>
-     </div>
+    <>
+      <div class="wrapper">
+        <div class="top">
+          <div class="wrapper-inner">
+            <div class="box-inner">A</div>
+            <div class="box-inner">B</div>
+            <div class="box-inner">C</div>
+            <div class="box-inner">D</div>
+            <div class="box-inner">E</div>
+            <div class="box-inner">F</div>
+            <div class="box-inner">G</div>
+            <div class="box-inner">H</div>
+          </div>
+        </div>
+        <div class="box">1</div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="bottom">
+          <div class="wrapper-inner">
+            <div class="box-inner">A</div>
+            <div class="box-inner">B</div>
+            <div class="box-inner">C</div>
+            <div class="box-inner">D</div>
+            <div class="box-inner">E</div>
+            <div class="box-inner">F</div>
+            <div class="box-inner">G</div>
+            <div class="box-inner">H</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
