3e73bdbef927881e1e5a385b3d04f41219f93e4f
shivamnarware <shivamnarware97@gmail.com>
2022-01-10 11:39:48 +0530

    "added rook"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import './App.css'
 import Box from "./Box";
 
 
 function App() {
   const arr = [];
-  for (let i = 1; i <= 64; i++) {
+  for (let i = 0; i < 64; i++) {
     arr.push(i);
   }
 
-  const [res,setRes]=useState();
+
+  const [res, setRes] = useState();
+  const [queenarr, setQueenArr] = useState();
+
+  const Queen = (res) => {
+    let a = [];
+    let x = parseInt(res / 8);
+    for (let i = 8 * x; i < 8 * (x + 1); i++) {
+      a.push(i);
+    }
+    let y = res % 8;
+    for (let j = 1; j <= 8; j++) {
+      a.push(y)
+      y += 8;
+    }
+    setQueenArr(a);
+  }
 
   return (
     <>
@@ -27,10 +43,9 @@ function App() {
           </div>
         </div>
 
-
         {
           arr.map((idx) => (
-            <Box key={idx} i={idx} set={setRes} res={res}/>
+            <Box key={idx} i={idx} set={setRes} pos={queenarr} queen={Queen} />
           ))
         }
 
--- src/Box.js
+++ src/Box.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useState } from 'react/cjs/react.development';
 
-function Box({ i,set,res }) {
+function Box({ i, set, pos,queen}) {
+    
     const clickedHandler = (e) => {
         e.preventDefault();
         const val = e.target.innerHTML;
         set(val);
-    
+        queen(val);
     }
+ 
     let x;
-    if (i>=res && i<=64) {
+    if (pos && pos.includes(i)) {
         x = <div onClick={clickedHandler} className="change">{i}</div>
     } else {
         x = <div onClick={clickedHandler} className="box">{i}</div>
